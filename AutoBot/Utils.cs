0fbdb8b065774a0147119cb08d32a3c2c059f6ce
joseph <josephbartholomew@outlook.com>
2020-11-02 18:32:01 -0400

    fixes

--- AutoBot/Utils.cs
+++ AutoBot/Utils.cs
@@ -1874,6 +1874,7 @@ namespace AutoBot
 
                 using (var ctx = new CoreEntitiesContext())
                 {
+                    ctx.Database.CommandTimeout = 0;
                     var contacts = ctx.Contacts.Where(x => x.Role == "PO Clerk" || x.Role == "Developer").Select(x => x.EmailAddress).ToArray();
                     var lst = ctx.TODO_SubmitDocSetWithIncompleteInvoices
                         .Where(x => x.ApplicationSettingsId == BaseDataModel.Instance.CurrentApplicationSettings.ApplicationSettingsId).ToList()
