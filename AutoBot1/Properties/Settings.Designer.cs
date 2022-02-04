4a2044549e1139ad8ddfd6ac9cb84b1d3d9be416
joseph <josephbartholomew@outlook.com>
2022-02-04 11:24:15 -0400

    dev changes

--- AutoBot1/Properties/Settings.Designer.cs
+++ AutoBot1/Properties/Settings.Designer.cs
@@ -12,6 +12,7 @@ namespace AutoBot.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,6 +26,7 @@ namespace AutoBot.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool DevMode {
             get {
                 return ((bool)(this["DevMode"]));
             }
