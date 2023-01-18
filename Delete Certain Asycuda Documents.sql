fab8b3da60204f5cd25e7bab140b825868dd81dd
josep <josephbartholomew@outlook.com>
2023-01-17 20:24:26 -0400

    dev changes

--- Delete Certain Asycuda Documents.sql
+++ Delete Certain Asycuda Documents.sql
@@ -3,3 +3,10 @@ where ASYCUDA_Id in ( select ASYCUDA_Id from AsycudaDocument where ReferenceNumb
 
 delete from xcuda_ASYCUDA
 where ASYCUDA_Id in (select ASYCUDA_Id from AsycudaDocument where ReferenceNumber like '%march 2021%' and ImportComplete = 0)
+
+
+delete from xcuda_Item
+where ASYCUDA_Id in ( select ASYCUDA_Id from AsycudaDocument where RegistrationDate < '9/24/2018' )
+
+delete from xcuda_ASYCUDA
+where ASYCUDA_Id in (select ASYCUDA_Id from AsycudaDocument where RegistrationDate < '9/24/2018')
